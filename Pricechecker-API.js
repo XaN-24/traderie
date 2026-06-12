f6f032fc65802e8d0b122f63d0babda658a58ca9
Max <xanicpo@gmail.com>
2026-06-12 12:08:55 +0200

    Added metadata to script

--- Pricechecker-API.js
+++ Pricechecker-API.js
@@ -1,2 +1,13 @@
-alert("Ура! VS Code и Tampermonkey синхронизированы!");
-console.log("test button");
+// ==UserScript==
+// @name         Pricechecker
+// @namespace    http://tampermonkey.net/
+// @version      0.1
+// @description  Скрипт для проверки цен
+// @author       XaN-24
+// @match        https://traderie.com/*
+// @updateURL    https://raw.githubusercontent.com/XaN-24/traderie/refs/heads/main/Pricechecker-API.js
+// @downloadURL  https://raw.githubusercontent.com/XaN-24/traderie/refs/heads/main/Pricechecker-API.js
+// ==/UserScript==
+
+// Твой код ниже:
+alert("GitHub подключен!");
